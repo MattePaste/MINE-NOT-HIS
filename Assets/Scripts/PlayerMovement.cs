7d5519f92ef20e33565cd9c69225cdb8be44ad88
unknown <dchrisu@yahoo.com>
2018-09-12 22:19:46 -0500

    Initial

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    // Call Rigidbody component 
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Call FixedUpdate instead of Update
+    // We dont want to mess with physics
+    void FixedUpdate ()
+    {
+        //Add a forward force that can be changed in Unity IDE
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
